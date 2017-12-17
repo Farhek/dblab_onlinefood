5d2c3937362cffa8301965b8c52a1ee00b7fe467
Farhek <hekmat.farzad@gmail.com>
2017-12-17 15:58:43 +0330

    State FoodName added

---
+++ src/StateFoodName.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.api.objects.Update;
+
+public class StateFoodName implements State {
+
+    @Override
+    public void message() {
+
+    }
+
+    @Override
+    public void ChangeState(String state) {
+    }
+
+    @Override
+    public void Validate(Update update) {
+
+    }
+}
